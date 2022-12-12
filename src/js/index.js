b15449e93fcbdf558e206af7080b785486091015
Ruslan Tendora <ruslantendora@gmail.com>
2022-12-13 01:31:06 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // const axios = require('axios').default;
-import './header/index.js';
+import './header/index';
 // import createCardMarkup from './templates/cocktails-card.hbs';
 // console.log(createCardMarkup);
